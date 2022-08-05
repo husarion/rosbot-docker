03e18cebac2aac5cc0dcf7ed60604c6f5f154eba
pkowalsk1 <kowalski.pawel.r@gmail.com>
2022-08-05 10:02:04 +0200

    map-save.sh | free thresh added & map saving path changed

--- demo/map-save.sh
+++ demo/map-save.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -it slam-toolbox bash -c "source /opt/ros/\$ROS_DISTRO/setup.bash && ros2 run nav2_map_server map_saver_cli --fmt png -f map"
+docker exec -it slam-toolbox bash -c "source /opt/ros/\$ROS_DISTRO/setup.bash && ros2 run nav2_map_server map_saver_cli --free 0.15 --fmt png -f /maps/map"
