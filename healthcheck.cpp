4e6c0611690c610f9e7ad0f76bd054d0aa1683f3
rafal-gorecki <rafal.gorecki@husarion.com>
2023-11-17 14:26:01 +0100

    New version of Healthcheck

--- healthcheck.cpp
+++ healthcheck.cpp
@@ -1,32 +1,50 @@
-#include "cstdlib"
+#include "fstream"
 #include "nav_msgs/msg/odometry.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 using namespace std::chrono_literals;
 
-#define TOPIC_NAME "/odometry/filtered"
-#define TIMEOUT 2s
+#define LOOP_PERIOD 2s
+#define MSG_VALID_TIME 5s
 
-int msg_received = EXIT_FAILURE;
+std::chrono::steady_clock::time_point last_msg_time;
+
+void write_health_status(const std::string &status) {
+  std::ofstream healthFile("/health_status.txt");
+  healthFile << status;
+}
 
 void msg_callback(const nav_msgs::msg::Odometry::SharedPtr msg) {
   std::cout << "Message received" << std::endl;
-  msg_received = EXIT_SUCCESS;
-  rclcpp::shutdown();
+  last_msg_time = std::chrono::steady_clock::now();
 }
 
-void timeout_callback() {
-  std::cout << "Timeout" << std::endl;
-  rclcpp::shutdown();
+void healthy_check(const rclcpp::Node::SharedPtr &node) {
+  std::chrono::steady_clock::time_point current_time =
+      std::chrono::steady_clock::now();
+  std::chrono::duration<double> elapsed_time = current_time - last_msg_time;
+  bool is_msg_valid = elapsed_time.count() < MSG_VALID_TIME.count();
+
+  if (is_msg_valid) {
+    std::cout << "Health check: healthy" << std::endl;
+    write_health_status("healthy");
+  } else {
+    std::cout << "Health check: unhealthy" << std::endl;
+    write_health_status("unhealthy");
+  }
 }
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("healthcheck_node");
   auto sub = node->create_subscription<nav_msgs::msg::Odometry>(
-      TOPIC_NAME, rclcpp::SensorDataQoS(), msg_callback);
-  auto timer = node->create_wall_timer(TIMEOUT, timeout_callback);
+      "/odometry/filtered", rclcpp::SensorDataQoS(), msg_callback);
 
-  rclcpp::spin(node);
-  return msg_received;
+  while (rclcpp::ok()) {
+    rclcpp::spin_some(node);
+    healthy_check(node);
+    std::this_thread::sleep_for(LOOP_PERIOD);
+  }
+
+  return 0;
 }
---
+++ healthcheck.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+HEALTHCHECK_FILE="/health_status.txt"
+
+# Function to start the ROS 2 healthcheck node
+start_healthcheck_node() {
+    /ros_entrypoint.sh ros2 run healthcheck_pkg healthcheck_node &
+}
+
+if [ ! -f "$HEALTHCHECK_FILE" ]; then
+    echo "Healthcheck file not found. Starting ROS 2 healthcheck node..."
+    start_healthcheck_node
+    # Wait a bit to allow the node to start and write its initial status
+    sleep 2
+fi
+
+# Now check the health status
+if [ -f "$HEALTHCHECK_FILE" ]; then
+    status=$(cat "$HEALTHCHECK_FILE")
+    if [ "$status" == "healthy" ]; then
+        exit 0
+    else
+        exit 1
+    fi
+else
+    echo "Healthcheck file still not found. There may be an issue with the node."
+    exit 1
+fi
