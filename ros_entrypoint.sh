e614e1b8e1e59981912c16cb121a6716f306fd0c
dominikn <dominik.nwk@gmail.com>
2023-12-01 20:34:52 +0100

    combined.launch.py is working fine

---
+++ ros_entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+output=$(husarnet-dds singleshot) || true
+if [[ "$HUSARNET_DDS_DEBUG" == "TRUE" ]]; then
+  echo "$output"
+fi
+
+# Check if XRCE_DOMAIN_ID_OVERRIDE is unset or empty
+if [ -z "$XRCE_DOMAIN_ID_OVERRIDE" ]; then
+    # If ROS_DOMAIN_ID is set and not empty, set XRCE_DOMAIN_ID_OVERRIDE to its value
+    if [ -n "$ROS_DOMAIN_ID" ]; then
+        export XRCE_DOMAIN_ID_OVERRIDE="$ROS_DOMAIN_ID"
+    fi
+fi
+
+# setup ros environment
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+source  "/ros2_ws/install/setup.bash"
+source  "/ros2_ws_microros_agent/install/setup.bash"
+
+exec "$@"
