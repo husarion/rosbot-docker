63389a45d340ae599fee2094a5202cb4c53fa546
dominikn <dominik.nwk@gmail.com>
2023-12-29 12:11:55 +0100

    make ros_entrypoint universal

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -6,18 +6,9 @@ if [[ "$HUSARNET_DDS_DEBUG" == "TRUE" ]]; then
   echo "$output"
 fi
 
-# Check if XRCE_DOMAIN_ID_OVERRIDE is unset or empty
-if [ -z "$XRCE_DOMAIN_ID_OVERRIDE" ]; then
-    # If ROS_DOMAIN_ID is set and not empty, set XRCE_DOMAIN_ID_OVERRIDE to its value
-    if [ -n "$ROS_DOMAIN_ID" ]; then
-        export XRCE_DOMAIN_ID_OVERRIDE="$ROS_DOMAIN_ID"
-    fi
-fi
-
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
-source "/ros2_ws/install/setup.bash"
-source "/ros2_ws_healthcheck/install/setup.bash"
+test -f "/ros2_ws/install/setup.bash" && source "/ros2_ws/install/setup.bash"
 
 if [ -z "$USER" ]; then
     export USER=root
@@ -25,5 +16,4 @@ elif ! id "$USER" &>/dev/null; then
     useradd -ms /bin/bash "$USER"
 fi
 
-gosu $USER bash -c "ros2 run healthcheck_pkg healthcheck_node &"
 exec gosu $USER "$@"
