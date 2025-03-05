44b3cc3090158fb5881e8387b12c3279018df8d7
rafal-gorecki <rafal.gorecki@husarion.com>
2025-03-05 15:39:28 +0100

    Workaround fix

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
-HEALTHCHECK_FILE="/var/tmp/health_status.txt"
+# HEALTHCHECK_FILE="/var/tmp/health_status.txt"
 
-# Now check the health status
-if [ -f "$HEALTHCHECK_FILE" ]; then
-    status=$(cat "$HEALTHCHECK_FILE")
-    if [ "$status" == "healthy" ]; then
+# # Now check the health status
+# if [ -f "$HEALTHCHECK_FILE" ]; then
+#     status=$(cat "$HEALTHCHECK_FILE")
+#     if [ "$status" == "healthy" ]; then
+#         exit 0
+#     else
+#         exit 1
+#     fi
+# else
+#     echo "Healthcheck file still not found. There may be an issue with the node."
+#     exit 1
+# fi
+
+# FIXME: # <additional-user-commands> gosu commands are used before creation the user
+LOCKFILE="/tmp/script_lockfile"
+
+if [[ -f "$LOCKFILE" ]]; then
+    START_TIME=$(cat "$LOCKFILE")
+    CURRENT_TIME=$(date +%s)
+
+    if (( CURRENT_TIME - START_TIME >= 20 )); then
+        rm -f "$LOCKFILE"
         exit 0
     else
         exit 1
     fi
 else
-    echo "Healthcheck file still not found. There may be an issue with the node."
+    date +%s > "$LOCKFILE"
     exit 1
 fi
